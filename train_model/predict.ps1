10bf8c3519157534adecb570864f2a4cc5a186c5
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2023-05-22 22:49:02 +0200

    updated export/predict scripts

--- train_model/predict.ps1
+++ train_model/predict.ps1
@@ -1,1 +1,1 @@
-python ..\yolov5\detect.py --weights C:\Users\michael.mitter\Documents\Programming\thepigeoniator\yolov5\runs\train\exp11\weights\best.onnx --source C:\Users\michael.mitter\Documents\Programming\thepigeoniator\train_model\data\test\images\
+python ..\yolov5\detect.py --weights C:\Users\michael.mitter\Documents\Programming\thepigeoniator\yolov5\runs\train\exp15\weights\best.onnx --source C:\Users\michael.mitter\Documents\Programming\thepigeoniator\train_model\data\test\images\
