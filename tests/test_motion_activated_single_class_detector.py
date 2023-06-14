57a22095dcce265658f121885670efb2e66240cc
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2023-06-14 23:55:26 +0200

    added tests for motion activated single class image sequence

---
+++ tests/test_motion_activated_single_class_detector.py
@@ -0,0 +1,53 @@
+import pytest
+from unittest.mock import MagicMock
+from birdhub.detection import MotionActivatedSingleClassDetector
+
+@pytest.fixture
+def detectors():
+    """Create mock detector and motion detector."""
+    detector = MagicMock()
+    motion_detector = MagicMock()
+    return detector, motion_detector
+
+@pytest.fixture
+def frame():
+    """Create a mock frame."""
+    return MagicMock()
+
+def test_no_motion_no_detection(detectors, frame):
+    """Test that the detector is not called if no motion is detected."""
+    detector, motion_detector = detectors
+    motion_detector.detect.return_value = None
+    detector.detect.return_value = None
+
+    motion_activated_detector = MotionActivatedSingleClassDetector(detector, motion_detector)
+    motion_activated_detector.detect(frame)
+
+    detector.detect.assert_not_called()
+
+def test_motion_detected(detectors, frame):
+    """Test that the detector is called if motion is detected."""
+    detector, motion_detector = detectors
+    motion_detections = [MagicMock()]
+    motion_detector.detect.return_value = motion_detections
+    detector.detect.return_value = motion_detections
+
+    motion_activated_detector = MotionActivatedSingleClassDetector(detector, motion_detector)
+    motion_activated_detector.detect(frame)
+
+    detector.detect.assert_called_once_with(frame)
+
+def test_slack_time_no_motion(detectors, frame):
+    """Test that the detector is called if slack is larger than 0 and no motion is detected."""
+    detector, motion_detector = detectors
+    motion_detections = [MagicMock()]
+    motion_detector.detect.side_effect = [motion_detections, None]
+    detector.detect.return_value = motion_detections
+
+    motion_activated_detector = MotionActivatedSingleClassDetector(detector, motion_detector, slack=10)
+    motion_activated_detector.detect(frame)
+    detector.detect.assert_called_once_with(frame)
+    detector.reset_mock()
+    
+    motion_activated_detector.detect(frame)
+    detector.detect.assert_called_once_with(frame)
