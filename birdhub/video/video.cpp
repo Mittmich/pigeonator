e413876988486e90a3fee25ee6f3de95ce7006ec
Michael Mitter <michael_mitter@hotmail.com>
2024-09-11 21:27:44 +0200

    formatting of tests

--- birdhub/video/video.cpp
+++ birdhub/video/video.cpp
@@ -4,35 +4,46 @@
 
 #include "video.hpp"
 
-
-ImageStore::ImageStore(int size) {
+ImageStore::ImageStore(int size)
+{
     // check if size is negative
-    if (size <= 0) {
+    if (size <= 0)
+    {
         throw std::invalid_argument("Size must be greater than 0.");
     }
     // check if size is too large
-    if (size > MAX_IMAGE_STORE_SIZE) {
+    if (size > MAX_IMAGE_STORE_SIZE)
+    {
         throw std::invalid_argument("Size must be less than " + std::to_string(MAX_IMAGE_STORE_SIZE) + ".");
     }
     this->size = size;
 }
 
-void ImageStore::put(std::time_t timestamp, cv::Mat image) {
+void ImageStore::put(std::time_t timestamp, cv::Mat image)
+{
     // check if image is empty
-    if (image.empty()) {
+    if (image.empty())
+    {
         throw std::invalid_argument("Image must not be empty.");
     }
     // check if store is full
-    if (this->timestamp_queue.size() >= this->size) {
+    if (this->timestamp_queue.size() >= this->size)
+    {
+        // get front element from queue and delete it from map
+        std::time_t front = this->timestamp_queue.front();
+        this->image_map.erase(front);
+        // delete front element from queue
         this->timestamp_queue.pop_front();
     }
     this->timestamp_queue.push_back(timestamp);
     this->image_map[timestamp] = image;
 }
 
-cv::Mat ImageStore::get(std::time_t timestamp) {
+cv::Mat ImageStore::get(std::time_t timestamp)
+{
     // check if timestamp is in store
-    if (this->image_map.count(timestamp) == 0) {
+    if (this->image_map.count(timestamp) == 0)
+    {
         throw std::invalid_argument("Timestamp not found in store.");
     }
     return this->image_map[timestamp];
